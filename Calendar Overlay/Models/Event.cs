b37e6b48e623a4e9b403c04994687b09d984efcd
Sorin <jordache_sor@yahoo.com>
2022-01-18 10:29:57 +0200

    added mock events generator

---
+++ Calendar Overlay/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calendar_Overlay.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndtDate { get; set; }
+
+        public Event(string name, DateTime startDate, DateTime endDate)
+        {
+            Name = name;
+            StartDate = startDate;
+            EndtDate = endDate;
+        }
+    }
+}
